1de8252176f421a6d137d13be5e133e0337ccc4c
Diana <dianasakaub@gmail.com>
2024-07-18 16:32:25 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ToDoList from './main.jsx';
 
 function App() {
-  const [quote, setQuote] = useState("Kanye once said")
-  useEffect(() => { //react hook
-    fetch('https://api.kanye.rest/')
-      .then((res) => {
-        const data = res.json()
-        return data;
-      })
-      .then((data) => {
-        console.log(data)
-        const retrievedQuote = data.quote 
-        console.log(retrievedQuote)
-        setQuote(data.quote)
-      })
 
-
-  }, [])
-
-  return (
-   <div>{quote}</div>
-  )
+    return (<ToDoList />)
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React, { useState } from 'react';
+import './App.css'; 
+
+function App() {
+  
+  const [containers, setContainers] = useState({
+    container1: "Container 1",
+    container2: "Container 2",
+    container3: "Container 3",
+    container4: "Container 4"
+  });
+
+  
+  const changeContent = (containerId, newContent) => {
+    setContainers(prevContainers => ({
+      ...prevContainers,
+      [containerId]: newContent
+    }));
+  };
+
+  return (
+    <div className="App">
+      <div className="top-container">
+        <div id="container1" className="flexible-container" onClick={() => changeContent("container1", "To-Do List Creator 1")}>
+          {containers.container1}
+        </div>
+        <div id="container2" className="flexible-container" onClick={() => changeContent("container2", "To-Do List Creator 2")}>
+          {containers.container2}
+        </div>
+        <div id="container3" className="flexible-container" onClick={() => changeContent("container3", "To-Do List Creator 3")}>
+          {containers.container3}
+        </div>
+      </div>
+
+      <div className="middle-container">
+        <div id="vertical1" className="vertical-container">
+          {containers.container1} (Vertical)
+        </div>
+        <div id="vertical2" className="vertical-container">
+          {containers.container2} (Vertical)
+        </div>
+      </div>
+
+      <div className="bottom-container">
+        <div id="container4" className="flexible-container" onClick={() => changeContent("container4", "New Content for Container 4")}>
+          {containers.container4}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
